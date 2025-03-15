02347344471dda46571721629c80516e0e0f020c
Arslan-git8840 <arshu09868@gmail.com>
2025-03-15 12:16:57 +0530

    ok

--- src/lib/drizzleActions.js
+++ src/lib/drizzleActions.js
@@ -237,14 +237,14 @@ export const getBorrowedBook = async () => {
     // });
 
     // Schedule a task to trigger an event
-    await client.schedules.create({
-      destination: "https://university-library-tan.vercel.app/api/workflow", // Your API endpoint
-      cron: "*/5 * * * *", // Runs every 5 minutes (for testing)
-      method: "POST", // Ensure this matches your API method
-      body: JSON.stringify({
-        message: "This is a test trigger", // Adjust payload as needed
-      }),
-    });
+    // await client.schedules.create({
+    //   destination: "https://university-library-tan.vercel.app/api/workflow", // Your API endpoint
+    //   cron: "*/5 * * * *", // Runs every 5 minutes (for testing)
+    //   method: "POST", // Ensure this matches your API method
+    //   body: JSON.stringify({
+    //     message: "This is a test trigger", // Adjust payload as needed
+    //   }),
+    // });
 
     if (!response || response.length === 0) {
       return { success: false, message: "No borrowed books found." };
