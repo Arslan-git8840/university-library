0710797b97ca562b7836657f303ab4655c4c3bd9
Arslan-git8840 <arshu09868@gmail.com>
2025-05-04 19:15:04 +0530

    using force-dynamic

--- src/app/admin/users/page.jsx
+++ src/app/admin/users/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import UserTable from '@/components/admin/userTable'
 import { db } from '@/db/drizzle'
 import { users } from '@/db/schema'
@@ -7,10 +8,10 @@ async function Users() {
   const userList = await db.select().from(users);
   return (
     <div className='lg:p-8 px-3 py-6'>
-        <h1 className='text-2xl font-semibold mb-4 text-primary-admin'>All Users</h1>
-        <div className='lg:py-4 lg:px-8 px-2'>
-            <UserTable userList={userList} />
-        </div>
+      <h1 className='text-2xl font-semibold mb-4 text-primary-admin'>All Users</h1>
+      <div className='lg:py-4 lg:px-8 px-2'>
+        <UserTable userList={userList} />
+      </div>
     </div>
   )
 }
